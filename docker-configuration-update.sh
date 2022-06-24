2253c3eaa016db1ee7853c4ce7506f047a4d729e
madebyTimo <t.schnaible@madebytimo.de>
2022-06-24 14:42:01 +0200

    add start of test-build

--- docker-configuration-update.sh
+++ docker-configuration-update.sh
@@ -8,3 +8,5 @@ chmod +x docker-configuration-update.sh
 # start new configuration
 docker-compose up --remove-orphans -d
 docker restart reverse-proxy
+docker-compose -f docker-compose-test.yaml
+docker restart reverse-proxy-test
